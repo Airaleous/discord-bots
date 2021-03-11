0a6cbf8a925bc8c2e337060485fcedafb19484f3
Airaleous <48191636+Airaleous@users.noreply.github.com>
2021-03-11 08:56:33 -0800

    Update secret-token-example.py

--- code/secret-token-example.py
+++ code/secret-token-example.py
@@ -1,3 +1,7 @@
+#Your bot token is how you control your bot. It's important that no one discover your bot token (It can be rest in the developer dashboard).
+#A good way to avoid leaking your token is loading it from an external file (Line 6) and storing it a token variable.
+#From there, the token variable can be used in place of your actual token in your code
+
 import discord
 
 token = open("token.txt","r").read() #Tokens are secret. Opens the token text file and stores it in the token variable
