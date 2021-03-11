b4e2a3112ad7ed984e3f76d7f67ca3d9bfd34c2c
Airaleous <48191636+Airaleous@users.noreply.github.com>
2021-03-11 08:45:19 -0800

    Update ping.py

--- code/ping.py
+++ code/ping.py
@@ -5,10 +5,6 @@ client = discord.Client() #Defines client
 @client.event
 async def on_message(ctx): #Waits for a message to be sent. 'ctx' is the message object
     if ctx.content == "ping!": #Checks message content for phrase
-        await ctx.delete()
         await ctx.channel.send("Pong!") #Replies "Pong!" to the channel the message was sent in
-    elif ctx.content == "notping!": #Checks message content for phrase
-        await ctx.channel.send("NotPing!") #Replies "Pong!" to the channel the message was sent in
-
 
 client.run('Your Token!') #Initializes the bot. Get your token from the Developer dashboard
